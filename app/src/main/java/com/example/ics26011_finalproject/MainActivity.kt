123e6f1a580e1591c3c8144e2adb24b320d653d6
peuransiseu <90952718+peuransiseu@users.noreply.github.com>
2023-11-17 18:13:25 +0800

    FinalProject Files

---
+++ app/src/main/java/com/example/ics26011_finalproject/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.ics26011_finalproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart(){
+        super.onStart()
+
+        Toast.makeText(this, "In onStart", Toast.LENGTH_SHORT).show();
+
+        Log.i("info", "In onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Toast.makeText(this, "In onResume", Toast.LENGTH_SHORT).show()
+
+        Log.i("info", "In onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Toast.makeText(this, "In onPause", Toast.LENGTH_SHORT).show()
+
+        Log.i("info", "In onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Toast.makeText(this, "In onStop", Toast.LENGTH_SHORT).show()
+
+        Log.i("info", "In onStop")
+    }
+
+
+}
